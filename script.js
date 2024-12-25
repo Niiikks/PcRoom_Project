cd441947b3965721a647ffac8840669e2d169b79
Nika Melkoniani <nmelkoniani@seu.edu.ge>
2024-12-26 00:34:47 +0400

    responsive and dropdown

--- script.js
+++ script.js
@@ -31,3 +31,43 @@ nextButton.addEventListener('click', () => {
     updateSlider();
     interval = setInterval(autoSlide, 3000);
 })
+
+// dropdown button
+let p = 50;
+let int;
+function daxurva(){
+    let container = document.querySelector('.dropdown-content');
+    p -= 3;
+    container.style.height = `${p}px`;
+    if(p <= 8) {
+        container.style.paddingTop = `${p}px`;
+    }
+    if(p <= 0) {
+        clearInterval(int);
+        container.style.display = 'none';
+    }
+}
+function gaxsna(){
+    let container = document.querySelector('.dropdown-content');
+    container.style.display = 'block';
+    p += 3;
+    container.style.height = `${p}px`;
+    if(p >= 10) {
+        container.style.paddingTop = `10px`;
+    }
+    if(p >= 50) {
+        clearInterval(int);
+    }
+}
+
+
+
+function start() {
+    if(p<=0) {
+        int = setInterval(gaxsna, 1);
+    }
+    else {
+        int = setInterval(daxurva, 1);
+    }
+}
+
